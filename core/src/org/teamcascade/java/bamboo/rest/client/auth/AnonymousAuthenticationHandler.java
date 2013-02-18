427d8d8e1aefd08d29d5aa06ba02089e0fd189a5
pcharbon <hz_ducky@hotmail.com>
2013-02-18 08:00:52 -0500

    Started on Core module adaptation

---
+++ core/src/org/teamcascade/java/bamboo/rest/client/auth/AnonymousAuthenticationHandler.java
@@ -0,0 +1,37 @@
+/*
+* Copyright (C) 2013 Pascal Charbonneau
+* Original Copyright (C) 2012 Atlassian
+*
+* Original code adapted by Pascal Charbonneau for the BRJC library
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
+package org.teamcascade.java.bamboo.rest.client.auth;
+
+import com.atlassian.httpclient.api.Request;
+import org.teamcascade.java.bamboo.rest.client.api.AuthenticationHandler;
+
+/**
+ * Anonymous handler - no credentials passed to the server. Only anonymously accessible operations will be possible.
+ *
+ * @since v0.1
+ */
+public class AnonymousAuthenticationHandler implements AuthenticationHandler {
+
+	@Override
+	public void configure(Request request) {
+	}
+
+}
