1d513e69f87c62d41e3b11cc0e58e81911c474a6
pcharbon <hz_ducky@hotmail.com>
2013-02-18 21:06:00 -0500

    First compiled version - only includes Project/Plan REST points

--- api/src/main/java/org/teamcascade/java/bamboo/rest/client/api/domain/Project.java
+++ api/src/main/java/org/teamcascade/java/bamboo/rest/client/api/domain/Project.java
@@ -20,7 +20,7 @@
 
 package org.teamcascade.java.bamboo.rest.client.api.domain;
 
-import com.atlassian.jira.rest.client.api.OptionalIterable;
+
 import com.google.common.base.Objects;
 
 import javax.annotation.Nullable;
@@ -34,81 +34,22 @@ import java.util.Collection;
  * @since v0.1
  */
 public class Project extends BasicProject {
-	@Nullable
-	private final String description;
-	private final BasicUser lead;
-	@Nullable
-	private final URI uri;
-	private final Collection<Version> versions;
-	private final Collection<BasicComponent> components;
-	private final OptionalIterable<IssueType> issueTypes;
-	private final Collection<BasicProjectRole> projectRoles;
+	private final Collection<Plan> plans;
 
-	public Project(URI self, String key, String name, String description, BasicUser lead, URI uri,
-                   Collection<Version> versions, Collection<BasicComponent> components,
-                   OptionalIterable<IssueType> issueTypes, Collection<BasicProjectRole> projectRoles) {
+	public Project(URI self, String key, String name,Collection<Plan> plans) {
 		super(self, key, name);
-		this.description = description;
-		this.lead = lead;
-		this.uri = uri;
-		this.versions = versions;
-		this.components = components;
-		this.issueTypes = issueTypes;
-		this.projectRoles = projectRoles;
+		this.plans = plans;
 	}
 
-	/**
-	 * @return description provided for this project or null if there is no description specific for this project.
-	 */
-	@Nullable
-	public String getDescription() {
-		return description;
-	}
+
 
 	/**
-	 * @return the person who leads this project
+	 * @return plans defined for this project
 	 */
-	public BasicUser getLead() {
-		return lead;
+	public Iterable<Plan> getPlans() {
+		return plans;
 	}
 
-	/**
-	 * @return user-defined URI to a web page for this project, or <code>null</code> if not defined.
-	 */
-	@Nullable
-	public URI getUri() {
-		return uri;
-	}
-
-	/**
-	 * @return versions defined for this project
-	 */
-	public Iterable<Version> getVersions() {
-		return versions;
-	}
-
-	/**
-	 * @return components defined for this project
-	 */
-	public Iterable<BasicComponent> getComponents() {
-		return components;
-	}
-
-	/**
-	 * Getter for issueTypes
-	 *
-	 * @return the issueTypes defined for this project
-	 */
-	public OptionalIterable<IssueType> getIssueTypes() {
-		return issueTypes;
-	}
-
-	/**
-	 * @return basic definition of this project's roles.
-	 */
-	public Iterable<BasicProjectRole> getProjectRoles() {
-		return projectRoles;
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -116,12 +57,7 @@ public class Project extends BasicProject {
 	@Override
 	protected Objects.ToStringHelper getToStringHelper() {
 		return super.getToStringHelper().
-				add("description", description).
-				add("lead", lead).
-				add("uri", uri).
-				add("components", components).
-				add("issueTypes", issueTypes).
-				add("versions", versions);
+				add("plans", plans);
 	}
 
 	@Override
@@ -129,18 +65,13 @@ public class Project extends BasicProject {
 		if (o instanceof Project) {
 			Project that = (Project) o;
 			return super.equals(that)
-					&& Objects.equal(this.lead, that.lead)
-					&& Objects.equal(this.uri, that.uri)
-					&& Objects.equal(this.description, that.description)
-					&& Objects.equal(this.components, that.components)
-					&& Objects.equal(this.issueTypes, that.issueTypes)
-					&& Objects.equal(this.versions, that.versions);
+					&& Objects.equal(this.plans, that.plans);
 		}
 		return false;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hashCode(super.hashCode(), description, lead, uri);
+		return Objects.hashCode(super.hashCode());
 	}
 }
